e474a0662ac0a360a800492c0cf2fd1398db2459
Glalyyy <gamcth10a2.c3truonglongtay@gmail.com>
2023-11-26 00:31:18 +0700

    5.0.0.0

--- EdithTour/Edithtour/Edithtour/Controllers/AccountController.cs
+++ EdithTour/Edithtour/Edithtour/Controllers/AccountController.cs
@@ -21,5 +21,9 @@ namespace EdithTour.Controllers
         {
             return View();
         }
+        public ActionResult Information()
+        {
+            return View();
+        }
     }
 }
